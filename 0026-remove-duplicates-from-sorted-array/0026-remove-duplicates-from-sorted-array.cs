4b0a196340319774ccb28cffd334f25b404d8022
ErliMyzeqari <73100485+ErliMyzeqari@users.noreply.github.com>
2024-09-26 00:06:22 +0300

    Time: 140 ms (5.41%), Space: 50 MB (62.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,17 +1,16 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         
-        if(nums.Length <=1){
-            return nums.Length;
-        }
+        
+          
+        
 
         int j = 1;
 
         for(int i = 1; i < nums.Length; i++){
             
             if(nums[i] != nums[i-1]){
-                nums[j] = nums[i];
-                j++;
+                nums[j++] = nums[i];             
             }
             
         }
